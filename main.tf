2f15fa8607673b984f51f294c4ef6392e71fea0b
Maddy Hodges <maddy.hodges@hashicorp.com>
2022-11-14 11:42:45 -0500

    added v11 tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "random_id" "random" {
+  keepers = {
+    uuid = "${uuid()}"
+  }
+
+  byte_length = 8
+}
+
+data "external" "example" {
+  program = ["/bin/sh", "-c", "sleep 5 && echo {}"]
+}
+
+output "random" {
+  value = "${random_id.random.hex}"
+}
