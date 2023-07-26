f1c2d4d3cd827205153cc2746878d7f93749808d
Maddy Hodges <maddy.hodges@hashicorp.com>
2023-07-26 13:40:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,24 +15,13 @@
 # }
 
 resource "random_id" "random" {
-  keepers = {
-    uuid = uuid()
+  keepers {
+    uuid = "${uuid()}"
   }
 
   byte_length = 8
 }
-
-data "external" "example" {
-  program = ["/bin/sh", "-c", "sleep 5 && echo {}"]
-}
-
+ 
 output "random" {
-  value = random_id.random.hex
-}
-
-
-resource "null_resource" "cluster" {
-  triggers = {
-    cluster_instance_ids = "test"
-  }
+  value = "${random_id.random.hex}"
 }
